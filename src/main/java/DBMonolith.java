bb82a14e577976d41e8c79823de13f3ebf7723a6
unknown <minghaozhou123@gmail.com>
2020-01-13 13:15:29 -0500

    JDBC conncetion template v1

---
+++ src/main/java/DBMonolith.java
@@ -0,0 +1,70 @@
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBMonolith {
+    public void loadMariaDBDriver(){
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+            System.out.println("Driver Loaded");
+        }
+        catch(Exception e){
+            throw new Error (e);
+        }
+    }
+    public Connection dbConnection(String dbVendor, String dbName){
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:"+dbVendor+"://localhost/"+dbName,"root","");
+            System.out.println("Database Connected");
+            return connection;
+        }
+        catch(Exception e){
+            throw new Error(e);
+        }
+    }
+    public Statement createStatement(Connection connection){
+        try {
+            Statement statement =  connection.createStatement();
+            return statement;
+        }
+        catch(Exception e){
+            throw new Error(e);
+        }
+    }
+    public ResultSet executeStatement(Statement statement, String sqlQuery){
+        try{
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+            return resultSet;
+        }
+        catch(Exception e){
+            throw new Error(e);
+        }
+    }
+    public static void main(String[] args) throws SQLException,ClassNotFoundException {
+
+        DBMonolith dbMonolith = new DBMonolith();
+
+        // first load JDBC driver
+        dbMonolith.loadMariaDBDriver();
+
+        // second connect to the database
+        Connection dbConnection=dbMonolith.dbConnection("mariadb","college");
+
+        // create DBStatement
+        Statement dbStatement = dbMonolith.createStatement(dbConnection);
+
+        // execute SQL query
+        String sqlQuery = "Select firstName, lastName, id From Student;";
+        ResultSet resultSet = dbMonolith.executeStatement(dbStatement, sqlQuery);
+
+        // prints out every row from query
+        while(resultSet.next()){
+            System.out.println(
+                    resultSet.getString(1)+"\t"+
+                            resultSet.getString(2)+"\t"+
+                            resultSet.getString(3)
+            );
+        }
+    }
+}
